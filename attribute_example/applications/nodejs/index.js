d8480c3d8ba98a2a0581877933ed80e0a999eb72
jayground <jayground8@gmail.com>
2024-10-27 18:05:59 +0900

    edit

--- attribute_example/applications/nodejs/index.js
+++ attribute_example/applications/nodejs/index.js
@@ -4,6 +4,7 @@ const opentelemetry = require('@opentelemetry/api');
 
 const counter1 = opentelemetry.metrics.getMeter('default').createCounter('empty');
 const counter2 = opentelemetry.metrics.getMeter('default').createCounter('null');
+const counter3 = opentelemetry.metrics.getMeter('default').createCounter('sample');
 
 function fibo(n) {
     if (n <= 2) return 1
@@ -14,6 +15,7 @@ http
   .createServer((req, res) => {
     counter1.add(1, { "hello": "" });
     counter2.add(1, { "hello": null });
+    counter3.add(1, { "hello": "world" });
     const result = fibo(38)
     res.write(`${result}\n`);
     res.end();
